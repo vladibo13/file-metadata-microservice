6a971b38f04ffe7748d47ff28f4e10af620f660b
vladibo13 <vladibo13@gmail.com>
2024-08-13 12:44:40 +0300

    added logic for file upload

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,7 +1,6 @@
 import asyncHandler from "../middlewares/async.middleware.js";
-import Example from "../models/example.model.js";
 
 export const exampleHandler = asyncHandler(async (req, res) => {
-  const examples = await Example.find();
-  res.status(200).json({ msg: "test", examples });
+  const { file } = req;
+  res.status(200).json({ msg: "upload success", file });
 });
--- routes/upload.router.js
+++ routes/upload.router.js
@@ -1,8 +1,20 @@
 import express from "express";
 import { exampleHandler } from "../controllers/upload.controller.js";
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 const router = express.Router();
 
-router.route("/upload").post(exampleHandler);
+router.route("/upload").post(upload.single("upfile"), exampleHandler);
 
 export default router;
